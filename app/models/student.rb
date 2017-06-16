2277f23699d695e7fd310817d1a3588b23c2c128
a12462004 <a12462004@gmail.com>
2017-06-16 04:59:55 -0400

    finish commit

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,19 @@
 class Student < ApplicationRecord
+  def show_grade
+      if fraction >= 90
+        "S級(#{fraction})"
+      elsif fraction > 80  && fraction < 90
+        "A級(#{fraction})"
+     
+      elsif fraction > 70  && fraction < 80
+        "B級(#{fraction})"
+     
+      elsif fraction > 60  && fraction < 70
+        "C級(#{fraction})"
+
+      else
+        "D級(#{fraction})"
+      end
+      
+  end
 end
